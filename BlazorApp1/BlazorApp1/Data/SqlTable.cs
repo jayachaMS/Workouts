d33d148a5d867b9993b31b73c3a3207ea2166106
JAYACHANDRAN JAGANNATHAN <jaya84@outlook.com>
2020-01-02 17:33:48 +0530

    ADDED: Blazor - sample project (layout)

---
+++ BlazorApp1/BlazorApp1/Data/SqlTable.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace BlazorApp1
+{
+	public class SqlTable
+	{
+		#region Constants
+		private static readonly DataTable tabInsurance = new DataTable("EDINS");
+		#endregion
+
+		#region Publics
+		public static DataTable GetDataTableToBind()
+		{
+			using(SqlConnection connection = new SqlConnection("Data Source=MS-00502;Initial Catalog=BlazorEducation;User ID=sa;Password=password-123"))
+			{
+				string strQuery = "select * from EDINS";
+
+				connection.Open();
+				SqlDataAdapter dataAdapter = new SqlDataAdapter(strQuery, connection);
+				connection.Close();
+
+				dataAdapter.Fill(tabInsurance);
+			}
+
+			return tabInsurance;
+		}
+		#endregion
+	}
+}
