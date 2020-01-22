675cedf28181112dad47217b59dd27408190edc4
JAYACHANDRAN JAGANNATHAN <jaya84@outlook.com>
2020-01-22 11:35:49 +0530

    ADDED: File mised

---
+++ BlazorApp1/BlazorApp1/Pages/MyRazors/todo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorApp1
+{
+	public class ToDoClass : ComponentBase
+	{
+		#region Fields
+		protected readonly IList<MyDoTo> todos = new List<MyDoTo>();
+		protected string newtodo;
+		#endregion
+
+		#region Constructors
+		public ToDoClass()
+		{
+			
+		}
+		#endregion
+
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+			base.BuildRenderTree(builder);
+		}
+
+		#region Privates
+		protected void AddNew()
+		{
+			if(string.IsNullOrEmpty(newtodo)) return;
+			todos.Add(new MyDoTo { Title = newtodo });
+			newtodo = string.Empty;
+		}
+		#endregion
+	}
+}
