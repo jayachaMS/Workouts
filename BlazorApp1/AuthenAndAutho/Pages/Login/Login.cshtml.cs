a1bd0f3209367ee547b3e616163030e311801378
JAYACHANDRAN JAGANNATHAN <jaya84@outlook.com>
2020-01-24 20:02:15 +0530

    ADDED: Login - Cookie - auth

---
+++ BlazorApp1/AuthenAndAutho/Pages/Login/Login.cshtml.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthenAndAutho.Pages.Login
+{
+	public class LoginModel : PageModel
+	{
+		#region Properties
+		[Required]
+		[BindProperty]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		[BindProperty]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		#endregion
+
+		#region Publics
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if(!(this.Email.ToLower().Equals("test@m-s.in") && this.Password.Equals("123456")))
+			{
+				return Page();
+			}
+
+			List<Claim> claims = new List<Claim>
+			                     {
+				                     new Claim(ClaimTypes.Name, "Test"),
+				                     new Claim(ClaimTypes.Email, this.Email)
+			                     };
+
+			ClaimsIdentity claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+			await this.HttpContext.SignInAsync(
+			                                   CookieAuthenticationDefaults.AuthenticationScheme,
+			                                   new ClaimsPrincipal(claimIdentity));
+
+			return LocalRedirect(this.Url.Content("~/host"));
+		}
+		#endregion
+	}
+}
---
+++ BlazorApp1/AuthenAndAutho/Pages/Login/Logout.cshtml.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthenAndAutho.Pages.Login
+{
+	public class LogoutModel : PageModel
+	{
+		#region Publics
+		public async Task<IActionResult> OnGetAsync(string returnUrl = null)
+		{
+			// Clear the existing external cookie
+
+			await this.HttpContext
+			          .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+			return LocalRedirect(this.Url.Content("~/"));
+		}
+		#endregion
+	}
+}
